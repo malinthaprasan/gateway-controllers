719463a590625de936894bc29d4560c857f1ad08
Thushani Jayasekera <thushanij.18@cse.mrt.ac.lk>
2026-03-24 20:45:54 +0530

    Update RespondPolicy

--- policies/respond/respond.go
+++ policies/respond/respond.go
@@ -22,6 +22,7 @@ import (
 	"fmt"
 	"regexp"
 
+	policyv1alpha2 "github.com/wso2/api-platform/sdk/core/policy/v1alpha2"
 	policy "github.com/wso2/api-platform/sdk/gateway/policy/v1alpha"
 )
 
@@ -196,4 +197,95 @@ func validateHeaderObjectKeys(headerMap map[string]interface{}, index int) error
 		}
 	}
 	return nil
+}
+
+// OnRequestHeaders returns an immediate response to the client in the request header phase.
+func (p *RespondPolicy) OnRequestHeaders(ctx *policyv1alpha2.RequestHeaderContext, params map[string]interface{}) policyv1alpha2.RequestHeaderAction {
+	statusCode := defaultStatusCode
+	if statusCodeRaw, ok := params["statusCode"]; ok {
+		parsedStatusCode, err := parseStatusCode(statusCodeRaw)
+		if err != nil {
+			return configErrorV2(err.Error())
+		}
+		statusCode = parsedStatusCode
+	}
+
+	body := []byte{}
+	if bodyRaw, ok := params["body"]; ok {
+		bodyString, ok := bodyRaw.(string)
+		if !ok {
+			return configErrorV2("body must be a string")
+		}
+		body = []byte(bodyString)
+	}
+
+	headers := make(map[string]string)
+	if headersRaw, ok := params["headers"]; ok {
+		headersList, ok := headersRaw.([]interface{})
+		if !ok {
+			return configErrorV2("headers must be an array")
+		}
+		for i, headerRaw := range headersList {
+			headerMap, ok := headerRaw.(map[string]interface{})
+			if !ok {
+				return configErrorV2(fmt.Sprintf("headers[%d] must be an object", i))
+			}
+			if err := validateHeaderObjectKeys(headerMap, i); err != nil {
+				return configErrorV2(err.Error())
+			}
+
+			nameRaw, ok := headerMap["name"]
+			if !ok {
+				return configErrorV2(fmt.Sprintf("headers[%d] missing required 'name' field", i))
+			}
+			name, ok := nameRaw.(string)
+			if !ok {
+				return configErrorV2(fmt.Sprintf("headers[%d].name must be a string", i))
+			}
+			if name == "" {
+				return configErrorV2(fmt.Sprintf("headers[%d].name cannot be empty", i))
+			}
+			if len(name) > headerNameMaxLen {
+				return configErrorV2(fmt.Sprintf("headers[%d].name must not exceed %d characters", i, headerNameMaxLen))
+			}
+			if !headerNamePattern.MatchString(name) {
+				return configErrorV2(fmt.Sprintf("headers[%d].name contains invalid characters", i))
+			}
+
+			valueRaw, ok := headerMap["value"]
+			if !ok {
+				return configErrorV2(fmt.Sprintf("headers[%d] missing required 'value' field", i))
+			}
+			value, ok := valueRaw.(string)
+			if !ok {
+				return configErrorV2(fmt.Sprintf("headers[%d].value must be a string", i))
+			}
+			if len(value) > headerValueMaxLen {
+				return configErrorV2(fmt.Sprintf("headers[%d].value must not exceed %d characters", i, headerValueMaxLen))
+			}
+
+			headers[name] = value
+		}
+	}
+
+	return policyv1alpha2.ImmediateResponse{
+		StatusCode: statusCode,
+		Headers:    headers,
+		Body:       body,
+	}
+}
+
+// configError returns a 500 error response for configuration issues
+func configErrorV2(message string) policyv1alpha2.ImmediateResponse {
+	errBody, _ := json.Marshal(map[string]string{
+		"error":   "Configuration Error",
+		"message": message,
+	})
+	return policyv1alpha2.ImmediateResponse{
+		StatusCode: 500,
+		Headers: map[string]string{
+			"content-type": "application/json",
+		},
+		Body: errBody,
+	}
 }
